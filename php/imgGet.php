b25a0d5d0b0bbb634ad7a1b20b442fe7a189f0d4
ndevalliere <nathalie.devalliere@gmail.com>
2018-05-28 21:19:27 +0100

    php source

--- php/imgGet.php
+++ php/imgGet.php
@@ -5,13 +5,13 @@ header('Access-Control-Allow-Origin: *');
 $count=0;
 $ar=array();
 $i=1;
-$g=scandir('../landingImg/');
+$g=scandir('landingImg/');
 
 foreach($g as $x)
 {
     if(is_dir($x))$ar[$x]=scandir($x);
-    else 
-    { 
+    else
+    {
         $count++;
         $ar[]=$x;
     }
