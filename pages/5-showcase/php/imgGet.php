68b7a1727a4c11e4a4b1a452da4a10bc6d23fbe3
ndevalliere <nathalie.devalliere@gmail.com>
2018-05-30 17:20:07 +0100

    why

---
+++ pages/5-showcase/php/imgGet.php
@@ -0,0 +1,25 @@
+<?php
+
+header('Access-Control-Allow-Origin: *');
+
+$count=0;
+$ar=array();
+$i=1;
+$g=scandir('../landingImg/');
+
+foreach($g as $x)
+{
+    if(is_dir($x))$ar[$x]=scandir($x);
+    else
+    {
+        $count++;
+        $ar[]=$x."*";
+    }
+}
+
+while($i <= $count)
+{
+    echo $ar[$i-1];
+    $i++;
+}
+?>
