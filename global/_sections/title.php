817aeaed4ff1c4d1a3c20f09d72a22043278719a
ndevalliere <nathalie.devalliere@gmail.com>
2018-06-04 00:31:06 +0100

    search images and hacked text, fix later

---
+++ global/_sections/title.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width">
+  <title>Sincerely,</title>
+  <link rel="stylesheet" type="text/css" href="./global/styles/restart_00.css"/>
+  <link rel="stylesheet" type="text/css" href="./global/styles/tempalte_00.css"/>
