79ccb0b29e089d40f6cebdf69191cd7d15e84402
benjchan <ben.chan@me.com>
2018-06-06 21:29:21 +0100

    tidy/labeled js file

--- pages/5-showcase/scripts/landingpagejs.js
+++ pages/5-showcase/scripts/landingpagejs.js
@@ -1,8 +1,3 @@
-
-var fontSize1;
-var fontSize;
-var screenWidth;
-var userId;
 var user;
 var uid;
 var isAnonymous;
@@ -10,15 +5,9 @@ var databaseUsers;
 var allUsers;
 var database;
 var usernames = [];
-var mouseAlign;
-var isClicked;
-var hovered;
-var direction;
-var direction2;
 var count;
 var xPos = 0;
 var yPos = 0;
-var clicked = false;
 var online;
 var filenames;
 var imgCount = 0;
@@ -58,23 +47,24 @@ $(document).ready(function(){
 
   database = firebase.database();
   allUsers = database.ref('users');
-  // allUsers.on('value', gotData);
 
   getAllStudents();
   getShowcaseImg();
   newUser();
 
-  var isMobile = false;
   // device detection
+  var isMobile = false;
   if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|ipad|iris|kindle|Android|Silk|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(navigator.userAgent)
       || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(navigator.userAgent.substr(0,4))) {
       isMobile = true;
   }
 
+  //Get/end mouse position data to firebase
   setTimeout(function(){
     setInterval(gotData, 250);
   },1500);
 
+  //mouse smoothing
   setTimeout(function(){
     setInterval(mouseFollow, 20);
     database.ref("userCount").on('value', function(data){
@@ -88,8 +78,10 @@ $(document).ready(function(){
     });
   },1000);
 
+  //check if page is in focus
   setInterval( checkPageFocus, 1000);
 
+  //adding firebase users. Create mice on new user.
   allUsers.on('child_added', function(data){
     var test2 = data.ge.path.n[1];
     if(!isMobile){
@@ -105,7 +97,7 @@ $(document).ready(function(){
     $( "#"+uid ).css({"pointer-events":"none","z-index":"100000"});
     $( "#"+uid ).html("·");
 
-
+    //move users own mouse
     $( window ).mousemove(function( event ) {
 
         yPos = ((event.pageY - $(window).scrollTop()) / $(window).height())*100;
@@ -115,12 +107,14 @@ $(document).ready(function(){
     });
 
   });
-
+  
+  //remove firebase mice on exit
   allUsers.on('child_removed', function(data){
     var test2 = data.ge.path.n[1];
     $( "#"+test2 ).remove();
   });
 
+  //get student profile page
   $('.students, #showcase-imgs').click(function(e){
     var that = $(this);
     var studentName = e.target.className.split(" ")[0];
@@ -132,6 +126,7 @@ $(document).ready(function(){
     });
   });
 
+  //pause showcase scroll on hover
   $('#showcase-imgs').hover(function(){
     imgReady = false;
     $('.showImg').each(function() {
@@ -147,6 +142,7 @@ $(document).ready(function(){
     }
   });
 
+  //clicking tags
   $('.type li').click(function(){
 
     var that = $(this);
@@ -163,6 +159,7 @@ $(document).ready(function(){
     showcaseTags = [];
     var tags = [];
 
+    //build list of active tags
     $('.type li').each(function(){
       var that = $(this);
       var type = that.parents('ul').data('type');
@@ -176,6 +173,7 @@ $(document).ready(function(){
       }
     });
 
+    //generate search tags
     var tagsLength = tags.length;
     var tagBlocks = '';
     for (var i = 0; i < tagsLength; i++){
@@ -183,12 +181,14 @@ $(document).ready(function(){
     }
     $('.results').html(tagBlocks);
 
+    //update search results
     updateSearch();
 
   });
 
 
-//use this to update content / value for filter function
+  //use this to update content / value for filter function
+  //search student names
   $('input').on('propertychange change click keyup input paste', function(){
     $('body').animate({scrollTop: ($(this).offset().top)}, 500);
     term = $(this).val().toLowerCase();
@@ -211,23 +211,25 @@ $(document).ready(function(){
     });
   });
 
+  //close student page
   $(document).on("click", ".close-button", function(){
-      $(".student").css("right", "-50%");
-      $('.showImg').each(function() {
-        var top = $(this).offset().top;
-        $(this).css("top", top + ($(window).height()*-1.5) + "px");
-      });
-      $("#student-blur").remove();
-      var style = $('<style id="student-blur">.showImg { filter: blur(0px); pointer-events: none; }</style>');
-      $('html > head').append(style);
-      setTimeout(function(){
-        $(".student").html("");
-        $("#student-blur").remove();
-        clicked = false;
-        imgReady = true;
-      },1000);
+    $(".student").css("right", "-50%");
+    $('.showImg').each(function() {
+      var top = $(this).offset().top;
+      $(this).css("top", top + ($(window).height()*-1.5) + "px");
     });
+    $("#student-blur").remove();
+    var style = $('<style id="student-blur">.showImg { filter: blur(0px); pointer-events: none; }</style>');
+    $('html > head').append(style);
+    setTimeout(function(){
+      $(".student").html("");
+      $("#student-blur").remove();
+      clicked = false;
+      imgReady = true;
+    },1000);
+  });
 
+  //tag side mouse scroll
   $('.row').mousemove(function(e){
     var that = $(this);
     var mouseX = e.pageX - that.offset().left - 100;
@@ -243,7 +245,7 @@ $(document).ready(function(){
 
 });
 
-
+//get list of students names and tags
 function getAllStudents(){
   var oReq = new XMLHttpRequest();
   oReq.onload = function() {
@@ -254,7 +256,7 @@ function getAllStudents(){
   oReq.send();
 }
 
-
+//load next showcase image
 function imgLoad() {
   firstRun = false;
   console.log('newImg');
@@ -280,7 +282,7 @@ function imgLoad() {
   });
 }
 
-
+//load student profile page
 function loadStudent(name) {
   console.log(name);
   var oReq = new XMLHttpRequest();
@@ -303,7 +305,7 @@ function loadStudent(name) {
   }, 200);
 }
 
-
+//update showcase filters/tags
 function updateSearch() {
   if(filtered.length === 0){
     $('li').removeClass('hiddenRemove');
@@ -315,7 +317,7 @@ function updateSearch() {
   getShowcaseImg();
 }
 
-
+//get updated showcase image list
 function getShowcaseImg(){
   var allTags = '';
   for (var i = 0; i < showcaseTags.length; i++) {
@@ -354,7 +356,7 @@ function getShowcaseImg(){
   oReq.send();
 }
 
-
+//add user to firebase
 function newUser() {
   firebase.auth().signInAnonymously().catch(function(error) {
     var errorCode = error.code;
@@ -375,7 +377,7 @@ function newUser() {
   });
 }
 
-
+//get/send mouse positions
 function gotData(){
   allUsers.once('value').then(function(snapshot) {
     userData = snapshot.val();
@@ -389,7 +391,7 @@ function gotData(){
   }
 }
 
-
+//mouse easing
 function mouseFollow(){
 
   for (var i = 0; i <= usernames.length-1; i++){
@@ -414,7 +416,7 @@ function mouseFollow(){
   }
 }
 
-
+//is the page in focus
 function checkPageFocus() {
 
   if ( document.hasFocus() ) {
@@ -425,7 +427,7 @@ function checkPageFocus() {
   }
 }
 
-
+//shuffle the showcase image array
 function shuffle(array) {
   var currentIndex = array.length, temporaryValue, randomIndex;
 
