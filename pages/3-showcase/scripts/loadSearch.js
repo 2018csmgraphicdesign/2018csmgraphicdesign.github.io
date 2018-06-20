22f7aab564583b7e9deddcdcdbc0d09aecd5cb8c
benjchan <ben.chan@me.com>
2018-06-20 12:44:10 +0100

    scrollFix

--- pages/3-showcase/scripts/loadSearch.js
+++ pages/3-showcase/scripts/loadSearch.js
@@ -1,19 +1,25 @@
 var dist = 0,
     notInput = false;
 
+var is_safari = /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
+
 $(window).on("scroll", function(){
   dist = $('html').scrollTop();
   // console.log(dist);
-  if(dist == 0){
+  if(is_safari || dist == 0){
+    console.log("chrome");
     dist = $('body').scrollTop();
-    $('body, html').animate({scrollTop: 0}, 250);
+    //$('body, html').animate({scrollTop: 0}, 250);
   }
+  console.log(dist);
   $(".container--search-input-top, .container--search").css({"background": "rgba(255,255,255, " + (dist/$(window).height()) + ")" });
   $('.title.fixed').css({"opacity": 1 - (dist/$(window).height()), "transition": "0.75s"});
 });
 
 $(".container--hover").mouseover(function(){
-  if( dist === 0 && !(notInput)){
+  console.log(dist);
+  if( dist === 0 && !notInput){
+    console.log("searchUp")
     $('body, html').animate({scrollTop: 50}, 250);
     //remove otherwise peeking happens after the user has already hovered over this area and discovered the peeking functionality
     // $(".container--search-input").removeClass("peekTop");
@@ -23,6 +29,7 @@ $(".container--hover").mouseover(function(){
 
 $("#frame").mouseover(function(){
   if( dist <= 50){
+    console.log("searchDown");
     $('body, html').animate({scrollTop: 0}, 250);
     $(".container--search-input").removeClass("peekTop");
     $(".container--search").removeClass("peekCont");
